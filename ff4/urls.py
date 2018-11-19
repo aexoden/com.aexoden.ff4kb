0c02e2471efed6906e964d84f662f7f7434cfbf0
Jason Lynch <jason@calindora.com>
2018-11-18 20:14:25 -0800

    Add an index for the entire site.
    Signed-off-by: Jason Lynch <jason@calindora.com>

--- ff4/urls.py
+++ ff4/urls.py
@@ -1,8 +1,10 @@
 from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
 
 urlpatterns = [
+	path('', TemplateView.as_view(template_name='index.html'), name='index'),
 	path('admin/', admin.site.urls),
 	path('routes/', include('ff4.routes.urls')),
 	path('tools/', include('ff4.tools.urls')),
