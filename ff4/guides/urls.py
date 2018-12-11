0f2f2f333e0e5753c4dad14b8297af8a42b14a89
Jason Lynch <jason@calindora.com>
2018-12-10 19:22:15 -0800

    Add the Any% No64 (Edge+Excalbur) guide.
    Signed-off-by: Jason Lynch <jason@calindora.com>

--- ff4/guides/urls.py
+++ ff4/guides/urls.py
@@ -5,5 +5,6 @@ app_name = 'guides'
 
 urlpatterns = [
 	path('', TemplateView.as_view(template_name='guides/index.html'), name='index'),
+	path('no64-excalbur/', TemplateView.as_view(template_name='guides/no64-excalbur.html'), name='no64-excalbur'),
 	path('nocw/', TemplateView.as_view(template_name='guides/nocw.html'), name='nocw'),
 ]
