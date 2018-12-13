e289affd012e275734e6bbe06d0411f9a8e35207
Jason Lynch <jason@calindora.com>
2018-12-12 19:48:50 -0800

    Add the Glitch Room Setup mini guide.
    Signed-off-by: Jason Lynch <jason@calindora.com>

--- ff4/guides/urls.py
+++ ff4/guides/urls.py
@@ -6,6 +6,7 @@ app_name = 'guides'
 urlpatterns = [
 	path('', TemplateView.as_view(template_name='guides/index.html'), name='index'),
 	path('cw-music/', TemplateView.as_view(template_name='guides/cw-music.html'), name='cw-music'),
+	path('mini-glitch-room-setup/', TemplateView.as_view(template_name='guides/mini-glitch-room-setup.html'), name='mini-glitch-room-setup'),
 	path('no64-excalbur/', TemplateView.as_view(template_name='guides/no64-excalbur.html'), name='no64-excalbur'),
 	path('nocw/', TemplateView.as_view(template_name='guides/nocw.html'), name='nocw'),
 ]
