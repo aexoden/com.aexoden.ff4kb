9a93f03ba07db484b8b1c913a6e03245733bef86
Jason Lynch <jason@calindora.com>
2018-12-12 20:44:32 -0800

    Add the inventory warping mini guide.
    Signed-off-by: Jason Lynch <jason@calindora.com>

--- ff4/guides/urls.py
+++ ff4/guides/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
 	path('', TemplateView.as_view(template_name='guides/index.html'), name='index'),
 	path('cw-music/', TemplateView.as_view(template_name='guides/cw-music.html'), name='cw-music'),
 	path('mini-glitch-room-setup/', TemplateView.as_view(template_name='guides/mini-glitch-room-setup.html'), name='mini-glitch-room-setup'),
+	path('mini-inventory-warping/', TemplateView.as_view(template_name='guides/mini-inventory-warping.html'), name='mini-inventory-warping'),
 	path('no64-excalbur/', TemplateView.as_view(template_name='guides/no64-excalbur.html'), name='no64-excalbur'),
 	path('nocw/', TemplateView.as_view(template_name='guides/nocw.html'), name='nocw'),
 ]
