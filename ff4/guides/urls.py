7ce24d2d3b60677be3e57484557e967d35712794
Jason Lynch <jason@calindora.com>
2018-12-15 19:50:27 -0800

    Add the SFC Any% NoCW guide.
    Signed-off-by: Jason Lynch <jason@calindora.com>

--- ff4/guides/urls.py
+++ ff4/guides/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
 	path('mini-inventory-warping/', TemplateView.as_view(template_name='guides/mini-inventory-warping.html'), name='mini-inventory-warping'),
 	path('no64-excalbur/', TemplateView.as_view(template_name='guides/no64-excalbur.html'), name='no64-excalbur'),
 	path('nocw/', TemplateView.as_view(template_name='guides/nocw.html'), name='nocw'),
+	path('sfc-nocw/', TemplateView.as_view(template_name='guides/sfc-nocw.html'), name='sfc-nocw'),
 ]
