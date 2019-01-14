87677c9074bce2e873909b1508d8fbbd86e4959a
Jason Lynch <jason@calindora.com>
2019-01-13 19:43:02 -0800

    Add a tool to check treasures in an SRAM file.
    Signed-off-by: Jason Lynch <jason@calindora.com>

--- ff4/tools/urls.py
+++ ff4/tools/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
 	path('nocw-gp-calculator/', TemplateView.as_view(template_name='tools/nocw-gp-calculator.html'), name='nocw-gp-calculator'),
 	path('nocw-igt-calculator/', TemplateView.as_view(template_name='tools/nocw-igt-calculator.html'), name='nocw-igt-calculator'),
 	path('seed-finder/', views.seed_finder, name='seed-finder'),
+	path('treasure-checker/', TemplateView.as_view(template_name='tools/treasure-checker.html'), name='treasure-checker'),
 ]
