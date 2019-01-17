139c51f7ccfe6f35d55597845d396565c95de591
Jason Lynch <jason@calindora.com>
2019-01-16 20:34:17 -0800

    Add a step routing guide.
    Signed-off-by: Jason Lynch <jason@calindora.com>

--- ff4/guides/urls.py
+++ ff4/guides/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
 	path('cw-music/', TemplateView.as_view(template_name='guides/cw-music.html'), name='cw-music'),
 	path('mini-glitch-room-setup/', TemplateView.as_view(template_name='guides/mini-glitch-room-setup.html'), name='mini-glitch-room-setup'),
 	path('mini-inventory-warping/', TemplateView.as_view(template_name='guides/mini-inventory-warping.html'), name='mini-inventory-warping'),
+	path('mini-step-routing/', TemplateView.as_view(template_name='guides/mini-step-routing.html'), name='mini-step-routing'),
 	path('no64-excalbur/', TemplateView.as_view(template_name='guides/no64-excalbur.html'), name='no64-excalbur'),
 	path('nocw/', TemplateView.as_view(template_name='guides/nocw.html'), name='nocw'),
 	path('sfc-nocw/', TemplateView.as_view(template_name='guides/sfc-nocw.html'), name='sfc-nocw'),
