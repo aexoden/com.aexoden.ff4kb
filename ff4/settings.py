0596dedee3d447804fffb8de2a3714ee3f49ae91
Jason Lynch <jason@calindora.com>
2019-02-06 22:40:37 -0800

    Add whitenoise for static files.
    Signed-off-by: Jason Lynch <jason@calindora.com>

--- ff4/settings.py
+++ ff4/settings.py
@@ -44,6 +44,7 @@ class Common(Configuration):
 
 	MIDDLEWARE = [
 		'django.middleware.security.SecurityMiddleware',
+		'whitenoise.middleware.WhiteNoiseMiddleware',
 		'django.contrib.sessions.middleware.SessionMiddleware',
 		'django.middleware.common.CommonMiddleware',
 		'django.middleware.csrf.CsrfViewMiddleware',
