d3e21d9821072a048ad0ee7c5080fb4a8238d6aa
Jason Lynch <jason@calindora.com>
2019-02-06 21:50:02 -0800

    Set STATIC_ROOT.
    Signed-off-by: Jason Lynch <jason@calindora.com>

--- ff4/settings.py
+++ ff4/settings.py
@@ -109,6 +109,7 @@ class Common(Configuration):
 
 	# Static files (CSS, JavaScript, Images)
 	# https://docs.djangoproject.com/en/2.0/howto/static-files/
+	STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 	STATIC_URL = '/static/'
 
 	STATICFILES_DIRS = [
