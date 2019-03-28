34a8206f1653de2df06a37e3cdce2789c42486d6
Jason Lynch <jason@calindora.com>
2019-03-28 10:14:13 -0700

    Update to the twin safe no64-excalbur routes.
    Signed-off-by: Jason Lynch <jason@calindora.com>

--- ff4/routes/views.py
+++ ff4/routes/views.py
@@ -51,8 +51,8 @@ ROUTES = {
 		'description': 'This route runs from the beginning of the game until Zeromus is defeated. Use of the 64-door glitch is banned. The step route begins after the Mist Clip. This particular version of the route relies on Edge as the primary damage dealer at the end of the game.',
 		'group': 'standard',
 		'enabled': True,
-		'twin_safe': False,
-		'status': '~95.54% Optimal'
+		'twin_safe': True,
+		'status': '~96.97% Optimal'
 	},
 
 	'premist': {
